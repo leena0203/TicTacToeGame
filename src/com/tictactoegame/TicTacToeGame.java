0be30e07daa7aa40081376724bf4ea2a8804bf52
leena0203 <leenasarode02@gmail.com>
2020-10-11 00:50:20 +0530

    Added: to check who wins the game

--- src/com/tictactoegame/TicTacToeGame.java
+++ src/com/tictactoegame/TicTacToeGame.java
@@ -9,6 +9,10 @@ public class TicTacToeGame {
 	public static char player;
 	public static int countEntry = 0;
 
+	enum Chance {
+		User, CompPlayer;
+	}
+
 	/**
 	 * Constructor
 	 */
@@ -74,15 +78,14 @@ public class TicTacToeGame {
 	}
 
 	/**
-	 * UC4 UC5
+	 * UC4
 	 * 
 	 * @param input
 	 * @param inputcolumn
 	 * @param board
 	 */
 
-	public static void markEntry(Scanner input, char[][] board) {
-
+	public static void markEntry(Scanner input, char symbol, Chance chance) {
 		int entryRow = 0;
 		int entryColumn = 0;
 		System.out.println("Enter the index for row and coulmn:");
@@ -90,41 +93,130 @@ public class TicTacToeGame {
 		entryColumn = input.nextInt();
 		if ((entryRow >= 0) && (entryRow < 3)) {
 			if ((entryColumn >= 0) && (entryColumn < 3)) {
-				if (board[entryRow][entryColumn] == ' ') {
-					board[entryRow][entryColumn] = player;
+				if (isBoardIndexFree(entryRow, entryColumn)) {
+					makeMove(entryRow, entryColumn, symbol, board);
 					printBorad(board);
-					countEntry++;
+					checkGameStatus(input, symbol, chance);
+					// countEntry++;
 
 				} else {
 					System.out.println("Already filled index");
-
+					markEntry(input, symbol, chance);
 				}
 			}
 		}
 	}
 
-	public static void tossToCheck(){
-		if (Math.random() < 0.5) {
-			System.out.println("Head is up:its currentPlayerMark");
-		} else {
-			System.out.println("Tails is up:its computer's turn");
+	/**
+	 * UC5
+	 * 
+	 * @param entryRow
+	 * @param entryColumn
+	 * @return
+	 */
+	public static boolean isBoardIndexFree(int entryRow, int entryColumn) {
+		boolean isFree = false;
+		if (board[entryRow][entryColumn] == ' ') {
+			isFree = true;
 		}
-
+		return isFree;
 	}
 
-	
+	/**
+	 * UC5
+	 * 
+	 * @param entryRow
+	 * @param entryColumn
+	 * @param symbol
+	 * @param board2
+	 */
+
+	private static void makeMove(int entryRow, int entryColumn, char symbol, char[][] board) {
+		board[entryRow][entryColumn] = symbol;
+		countEntry++;
+	}
+
+	public static int markEntryComputer() {
+		int mark = (int) Math.floor(Math.random() * 10) % 9 + 1;
+		/*
+		 * if (board[mark][mark] == ' ') { board[mark][mark] = computer; countEntry++;
+		 * printBorad(board);
+		 */
+		return mark;
+	}
+
+	/***
+	 * UC6
+	 * 
+	 * @return
+	 */
+
+	public static void tossToCheck() {
+
+		int random = (int) Math.floor((Math.random() * 2)) + 1;
+		if (random == 1) {
+			System.out.println("Player Start");
+			markEntry(input, player, Chance.User);
+		} else {
+			System.out.println("Computer Starts");
+			markEntry(input, computer, Chance.CompPlayer);
+		}
+	}
+
+	/**
+	 * UC7
+	 * 
+	 * @param board
+	 * @param symbol
+	 * @return
+	 */
+
+	public static boolean checkForWin(char[][] board, char symbol) {
+		boolean flag = false;
+		if (board[0][0] == board[0][1] && board[0][1] == board[0][2] && board[0][1] == symbol
+				|| board[0][0] == board[1][0] && board[1][0] == board[2][0] && board[1][0] == symbol
+				|| board[0][0] == board[1][1] && board[1][1] == board[2][2] && board[1][1] == symbol
+				|| board[0][1] == board[1][1] && board[1][1] == board[2][1] && board[1][1] == symbol
+				|| board[0][2] == board[1][2] && board[1][2] == board[2][2] && board[1][2] == symbol
+				|| board[0][2] == board[1][2] && board[1][1] == board[2][0] && board[1][1] == symbol
+				|| board[1][0] == board[1][1] && board[1][1] == board[1][2] && board[1][1] == symbol
+				|| board[2][0] == board[2][1] && board[2][1] == board[2][2] && board[2][1] == symbol) {
+			flag = true;
+		}
+		return flag;
+	}
+
+	public static boolean checkTie() {
+		boolean checkTie = false;
+		if (countEntry == 9) {
+			checkTie = true;
+		}
+		return checkTie;
+	}
+
+	private static void checkGameStatus(Scanner input2, char symbol, Chance chance) {
+		if (checkForWin(board, symbol)) {
+			if (symbol == player) {
+				System.out.println(" You WON!");
+			} else {
+				System.out.println(" Computer WON!");
+			}
+		} else if (checkTie()) {
+			System.out.println("Game came to a tie");
+		} else {
+			if (chance == Chance.User) {
+				markEntry(input, computer, Chance.CompPlayer);
+			} else {
+				markEntry(input, player, Chance.User);
+			}
+		}
+	}
 
 	public static void main(String[] args) {
 		TicTacToeGame game = new TicTacToeGame();
 		TicTacToeGame.createBoard();
 		TicTacToeGame.inputMark(input);
 		TicTacToeGame.tossToCheck();
-		while (true) {
-			markEntry(input, board);
-			if (countEntry == 9)
-				break;
-		}
 
 	}
-
 }
